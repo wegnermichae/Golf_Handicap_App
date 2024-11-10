47a5da8103f7dbd3c4b525cd0563b58f8c3c0266
wegnemichae <wegnermichael14@gmail.com>
2024-11-10 14:41:17 -0600

    Added submit button functionality and created method to calculate who gets extra strokes. This needs implementation yet.

---
+++ app/src/main/java/com/example/golfhandicapapp/Golfers.java
@@ -0,0 +1,40 @@
+package com.example.golfhandicapapp;
+
+public class Golfers {
+    private int id;
+    private String name;
+    private int handicap;
+    public Golfers(int id, String name, int handicap){
+        this.id = id;
+        this.name = name;
+        this.handicap = handicap;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getHandicap(){
+        return handicap;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+        }
+    public void setHandicap(int handicap){
+        this.handicap = handicap;
+    }
+
+    @Override
+    public String toString(){
+        return "Id: " + id + " Name: " + name + " Handicap: " + handicap;
+    }
+
+
+
+}
--- app/src/main/java/com/example/golfhandicapapp/PlayGolfActivity.java
+++ app/src/main/java/com/example/golfhandicapapp/PlayGolfActivity.java
@@ -23,6 +23,10 @@ public class PlayGolfActivity extends AppCompatActivity {
     ListView holeView;
     ImageButton dashButton, scoreButton, bagButton, courseButton, playerButton;
 
+    public void calculateExtraStrokes(){
+        //TODO: Calculate extra strokes for each golfer
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +43,40 @@ public class PlayGolfActivity extends AppCompatActivity {
         bagButton = findViewById(R.id.bagButton5);
         courseButton = findViewById(R.id.courseButton5);
         playerButton = findViewById(R.id.playerButton5);
+        submitButton = findViewById(R.id.submitButton);
+
+        courseName = findViewById(R.id.courseName);
+        golfer1Name = findViewById(R.id.golfer1Name);
+        golfer2Name = findViewById(R.id.golfer2Name);
+        golfer3Name = findViewById(R.id.golfer3Name);
+        golfer4Name = findViewById(R.id.golfer4Name);
+        golfer1Handicap = findViewById(R.id.golfer1Handicap);
+        golfer2Handicap = findViewById(R.id.golfer2Handicap);
+        golfer3Handicap = findViewById(R.id.golfer3Handicap);
+        golfer4Handicap = findViewById(R.id.golfer4Handicap);
+        golfer1Strokes = findViewById(R.id.golfer1Strokes);
+        golfer2Strokes = findViewById(R.id.golfer2Strokes);
+        golfer3Strokes = findViewById(R.id.golfer3Strokes);
+        golfer4Strokes = findViewById(R.id.golfer4Strokes);
+
+        holeView = findViewById(R.id.holeView);
+
+
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.submitButton) {
+                    Golfers golfers;
+                    try {
+                        golfers = new Golfers(-1, golfer1Name.getText().toString(), Integer.parseInt(golfer1Handicap.getText().toString()));
+                    } catch (Exception e) {
+                        golfers = new Golfers(-1, "Null", -1);
+                    }
+                }
+            }
+        });
 
 
         //the following listeners will allow for functionality of the specified button clicks
